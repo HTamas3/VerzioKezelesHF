f968477a1aa715ca521dcf1a4c41da474353f87f
HTamas3 <tamashahm99@gmail.com>
2020-09-27 23:05:39 +0200

    Megadható szám

--- Program.cs
+++ Program.cs
@@ -6,19 +6,21 @@ namespace VerzioKezelesHF
     {
         static void Main(string[] args)
         {
-            int[] szamok = new int[10];
+            Console.WriteLine("Add meg mennyi szám legyen!");
+            int szam = Convert.ToInt32(Console.ReadLine());
+            int[] szamok = new int[szam];
             Console.WriteLine("Adj meg 10 számot");
-            for(int i=0; i<10; i++)
+            for(int i=0; i<szam; i++)
             {
                 Console.Write("{0} szám: ",i+1);
                 szamok[i] = Convert.ToInt32(Console.ReadLine());
             }
             int osszeg = 0;
-            for(int i=0; i<10; i++)
+            for(int i=0; i<szam; i++)
             {
                 osszeg = osszeg + szamok[i];
             }
-            Console.WriteLine("Átlag: {0}", osszeg/10);
+            Console.WriteLine("Átlag: {0}", osszeg%szam);
             Console.ReadLine();
         }
     }
