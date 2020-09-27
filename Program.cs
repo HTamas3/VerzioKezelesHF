5264d4fdd00783df5ec0462b68e877f9eff56649
HTamas3 <tamashahm99@gmail.com>
2020-09-27 22:56:46 +0200

    Átlag

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace VerzioKezelesHF
     {
         static void Main(string[] args)
         {
-            
+            int[] szamok = new int[10];
+            Console.WriteLine("Adj meg 10 számot");
+            for(int i=0; i<10; i++)
+            {
+                Console.Write("{0} szám: ",i+1);
+                szamok[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            int osszeg = 0;
+            for(int i=0; i<10; i++)
+            {
+                osszeg = osszeg + szamok[i];
+            }
+            Console.WriteLine("Átlag: {0}", osszeg/10);
+            Console.ReadLine();
         }
     }
 }
